103b8f17ec5f9e36e3e045de46945b5c15721f96
williambrioli <brioli@outlook.com>
2025-11-17 16:17:36 -0300

    Update site-data.js

--- js/site-data.js
+++ js/site-data.js
@@ -3,8 +3,8 @@ window.SITE_DATA = {
     name: "William Brioli",
     title: "Psicanalista Clínico Integrativo",
     hoursHTML: "Segunda à sexta-feira — 8h00 às 22h00 (consulte agenda)",
-    phoneDisplay: "(18) 98809-2571",
-    phoneHref: "https://wa.me/5518988092571",
+    phoneDisplay: "",
+    phoneHref: "",
     contactButtonText: "Enviar mensagem",
     contactButtonWA: "generic"
   },
