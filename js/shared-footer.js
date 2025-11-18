b8bfcc3af63aae32ea49622033424c2ebd452e87
williambrioli <brioli@outlook.com>
2025-11-18 14:38:39 -0300

    Create shared-footer.js

---
+++ js/shared-footer.js
@@ -0,0 +1,10 @@
+document.getElementById("footerContainer").innerHTML = `
+  <p><strong>William Brioli</strong> é 
+  <strong>Psicanalista Clínico Integrativo</strong>, pós-graduado em 
+  <strong>Neurociência, Comunicação e Desenvolvimento Humano</strong>.<br>
+  Atende presencialmente em <strong>Ilha Solteira (SP)</strong> 
+  e <strong>online para todo o Brasil</strong>.<br>
+  Atua com <strong>psicoterapia individual, terapia de casal, 
+  empreendedorismo, carreira, desenvolvimento emocional e implementação de NR-1 para empresas.</strong>.
+  </p>
+`;
