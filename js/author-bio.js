ed31d64e17fa7adcd01828fa69967d0eb00d55e3
williambrioli <brioli@outlook.com>
2025-11-18 19:17:20 -0300

    Update author-bio.js

--- js/author-bio.js
+++ js/author-bio.js
@@ -1,9 +1,10 @@
 const authorBioHTML = `
-<p><strong>SOBRE O AUTOR</strong><br> William Brioli</strong> é <strong>Psicanalista Clínico Integrativo</strong>, 
+<p><strong>SOBRE O AUTOR</strong><br>
+William Brioli</strong> é <strong>Psicanalista Clínico Integrativo</strong>, 
 pós-graduado em <strong>Neurociência, Comunicação e Desenvolvimento Humano</strong>.<br>
 Atende presencialmente em <strong>Ilha Solteira (SP)</strong> e 
 <strong>online para todo o Brasil</strong>.<br>
-Atua com <strong>psicoterapia individual, terapia de casal, empreendedores, carreira 
+Atua com <strong>psicoterapia individual, terapia de casal, empreendedorismo, carreira 
 e desenvolvimento emocional</strong>.
 </p>
 `;
