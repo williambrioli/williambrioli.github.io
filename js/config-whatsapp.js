e1cbd5e694eea7cd9ff701d91c82ac46186dd404
williambrioli <brioli@outlook.com>
2025-12-10 14:48:25 -0300

    Update config-whatsapp.js

--- js/config-whatsapp.js
+++ js/config-whatsapp.js
@@ -35,9 +35,22 @@
     messages: {
       // Mensagem padrão ("generic").
       // Essa é usada caso nenhuma outra mensagem seja especificada.
-      generic: "Olá, William! Encontrei seu site e gostaria de conversar sobre atendimento.",
-      psicoterapiaindividuall: "Olá, William! Quero falar sobre Psicoterapia Individual."
-
+      generic: "Oi, William! Encontrei seu site e preciso conversar com você sobre...",
+      rodapegeneric: "William! Encontrei seu site e preciso conversar com você sobre...",
+      duvidageneric: "Olá, William! Encontrei seu site e preciso conversar com você sobre...",
+      psicoterapiaindividuall: "Olá, William! Quero falar sobre Psicoterapia Individual.",
+      psicoterapiabrevee: "Olá, William! Quero falar sobre Psicoterapia Breve.",
+      psicoterapiaunica: "Olá, William! Quero falar sobre Psicoterapia de Sessão Única.",
+      terapiadecasall: "Olá, William! Quero falar sobre Terapia de Casal.",
+      terapiaparaempreendedoress: "Olá, William! Quero falar sobre Terapia para empreendedores.",
+      mediadorextrajudiciall: "Olá, William! Quero falar sobre Mediação Extrajudicial.",
+      consultoriadesenvolvimentohumanoo: "Olá, William! Quero falar sobre Consultoria em Desenvolvimento Humano e Organizacional.",
+      transicaocarreira: "Olá, William! Quero falar sobre Orientação e Transição de Carreira.",
+      treinadorfinanc: "Olá, William! Quero falar sobre Orientação e Transição de Carreira.",
+      nr1: "Olá, William! Quero falar sobre implementação de NR-1.",
+      depoimentogeneric: "Olá, William! Vi o conteúdo do seu site e depoimento dos clientes e quero falar sobre atendimento com você.",
+      palestrageneric: "Olá, William! Vi que você é palestrante e quero falar sobre palestras."
+      
       
       // Exemplos de mensagens adicionais (comentadas).
       // Se quiser criar mais mensagens, basta copiar e alterar:
