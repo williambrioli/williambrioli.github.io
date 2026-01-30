3d37c7969be0add0b97ee3901094291b9be1a00a
williambrioli <brioli@outlook.com>
2026-01-30 14:34:02 -0300

    Create igc-config.js

---
+++ js/igc-config.js
@@ -0,0 +1,12 @@
+// js/igc-config.js
+
+window.IGC_CONFIG = {
+  APPS_SCRIPT_BASE:
+    "https://script.google.com/macros/s/AKfycbxB6mH2EqLyjQaUtMPMyx-GNLzHColazB0awBetRDG_Un3lCvYaF0tHRATIWOa7Iu6HsQ/exec",
+
+  APPS: {
+    ADMIN: "admin",
+    TOKEN: "token",
+    LOGIN: "login"
+  }
+};
