6dafd8ac6043f2be808ac053c4efb78f78496494
williambrioli <brioli@outlook.com>
2026-01-30 14:50:10 -0300

    Update igc-config.js

--- js/igc-config.js
+++ js/igc-config.js
@@ -10,3 +10,10 @@ window.IGC_CONFIG = {
     LOGIN: "login"
   }
 };
+
+// 🔹 Helper global (opcional, mas profissional)
+window.IGC = {
+  app(name) {
+    return `${window.IGC_CONFIG.APPS_SCRIPT_BASE}?app=${window.IGC_CONFIG.APPS[name]}`;
+  }
+};
