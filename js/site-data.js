3ade715c9433ef79edb27e1842530efc6a703e55
williambrioli <brioli@outlook.com>
2026-09-19 15:46:27 -0300

    Update button texts to 'Agendar consulta online'

--- js/site-data.js
+++ js/site-data.js
@@ -28,28 +28,28 @@ window.SITE_DATA = { // Cria um objeto global chamado SITE_DATA no objeto window
     modalCTA: "Agendar atendimento",              // botão dentro do modal — texto exibido no botão do modal.
     readMore: "Ler artigo completo",              // texto do botão "ler mais" nos cards da home/blog.
     closeModal: "Fechar",                          // texto do botão para fechar modais.
-    psicoterapiaindividual: "Consultar Online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
-    psicoterapiabreve: "Consultar Online",       // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
-    sessaounica: "Consultar Online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
-    terapiadecasal: "Consultar Online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
-    terapiaparaempreendedores: "Consultar Online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
-    orientacaoetransicaodecarreira: "Agendar Consulta",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
+    psicoterapiaindividual: "Agendar consulta online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
+    psicoterapiabreve: "Agendar consulta online",       // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
+    sessaounica: "Agendar consulta online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
+    terapiadecasal: "Agendar consulta online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
+    terapiaparaempreendedores: "Agendar consulta online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
+    orientacaoetransicaodecarreira: "Agendar consulta online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
     orientacaofinanceira: "Contratar Serviço",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
     nr1: "Contratar Serviço",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Como te ajudo.
-    planosaude: "Consultar Online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Dúvidas.
-    horariofuncionamento: "Consultar Online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Dúvidas.
-    precisoterapia: "Consultar Online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Dúvidas.
-    comofuncionaterapia: "Consultar Online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Dúvidas.
+    planosaude: "Agendar consulta online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Dúvidas.
+    horariofuncionamento: "Agendar consulta online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Dúvidas.
+    precisoterapia: "Agendar consulta online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Dúvidas.
+    comofuncionaterapia: "Agendar consulta online",        // 🔥 novos botões globais que alterar os boões dos ACOORDIONS Dúvidas.
     palestras: "Contratar Serviço",        // 🔥 novos botões globais que alterar os boões Palestras.
     mediadorextrajudicial: "Contratar Serviço",       
     consultor: "Contratar Serviço",        
-    depoimentos: "Consultar Online",
+    depoimentos: "Agendar consulta online",
     estrategiacomercialevendas: "Falar com o Consultor",
     posturagerencial: "Falar com o Consultor",
     liderancapratica: "Falar com o Consultor",
     acompanhargestores: "Falar com o Consultor",
     alinhamentoorganizacional: "Falar com o Consultor",
-    consultapresencial: "Consultar Presencial"
+    consultapresencial: "Agendar consulta presencial"
 
     
     
