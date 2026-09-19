3b42abb40f77f079932d19031ec65dcd024414c5
williambrioli <brioli@outlook.com>
2026-09-19 20:01:19 -0300

    Fix link generation in search results

--- js/search-bar.js
+++ js/search-bar.js
@@ -72,7 +72,7 @@
     // Se encontrou resultados → monta cada <li> usando map().
     results.innerHTML = list.map(post => `
       <li class="result-item">
-        <a href="/${post.html}">
+        <a href="/${post.html.replace(/^\//, '')}">
           <strong>${post.title}</strong><br>
           <small>${post.excerpt || ""}</small>
         </a>
